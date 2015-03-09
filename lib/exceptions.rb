1c4983931d3934fc497084ba8210b988a12b25fc
Douglas Rossignolli <douglas@kanamobi.com.br>
2015-03-09 10:16:45 -0300

    testing

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -2,9 +2,37 @@ exceptions = File.expand_path('../exceptions', __FILE__)
 $LOAD_PATH.unshift(exceptions)
 
 module Exceptions
-end
+  class Base < StandardError
+    attr_accessor :object, :type
 
-require 'exceptions/base'
+    # starts a new instance with an object
+    # @param [Object] object
+    def initialize object
+      self.object = object
+    end
+
+    # standard error for Models
+    # @param [ActiveRecord::Base] object
+    # @return [Exceptions::Base]
+    def self.build object
+      exception = new object
+      return exception
+    end
+
+    # return if is as simple error
+    # @return [Boolean]
+    def simple?
+      self.class.name.demodulize.tableize.singularize == "simple"
+    end
+
+    # return if is as model error
+    # @return [Boolean]
+    def model?
+      self.class.name.demodulize.tableize.singularize == "model"
+    end   
+  end
+
+end
 require 'exceptions/model'
 require 'exceptions/resource'
 require 'exceptions/simple'
